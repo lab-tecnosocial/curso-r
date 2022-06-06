eedf3d6a0e17e8449132558e249d722613b4483e
Alex Ojeda Copa <alex-roc@users.noreply.github.com>
2022-06-06 10:42:20 -0400

    script de procesamiento mejorado

--- procesar-docs.R
+++ procesar-docs.R
@@ -1,14 +1,24 @@
 library(rmarkdown)
-library(yaml)
+library(stringr)
 
 all_rmds <- list.files(recursive = T, pattern = "\\.Rmd$", full.names = T)
 
 
-knit_readme <- function(rmds){
-  if(!length(files > 1)){
+knit_readme <- function(folder){
+  rmds <- list.files(folder, pattern = ".Rmd$", full.names = T)
+  if(!length(rmds) > 1){
     render(rmds, output_file = "README.md")
   } else {
-    md_document()
+    file.create("temp.Rmd")
+    cat(cat("#", rmds), file = "temp.Rmd", sep = "\n")
+    for(i in seq_along(rmds)){
+      title <- yaml_front_matter(rmds[i])$title
+      body <- str_remove(rmds[i], "---(.|\n)*---")
+      cat("#", title, file = "temp.Rmd", append = T, sep = "\n")
+      cat(body, file = "temp.Rmd", sep = "\n", append = T)
+    }
+    render("temp.Rmd", output_format = "github_document", output_file = "README.md")
+    file.remove("temp.Rmd")
   }
 }
 
